df86e0e79898830050c02e4f05d56d07c8488ece
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-03-26 21:21:59 -0500

    modificacion

--- Lab02/DemoMetodoGenerico.java
+++ Lab02/DemoMetodoGenerico.java
@@ -27,8 +27,8 @@ class DemoMetodoGenerico {
             System.out.println("nums es igual a nums4");
 
         // Crea un array de double //A
-        //Double dvals[]={1.1,2.2,3.3,4.4,5.5}; //B
-        //if(igualArrays(nums,dvals)) //C
-        // System.out.println("nums es igual a dvals"); //D
+    Double dvals[]={1.1,2.2,3.3,4.4,5.5}; //B
+        if(igualArrays(nums,dvals)) //C
+            System.out.println("nums es igual a dvals"); //D
     }
 }
