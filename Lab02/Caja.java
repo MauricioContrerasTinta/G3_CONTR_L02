91c79ee94faff46f5b93fe73d3c69f0082e4e627
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-03-27 23:48:51 -0500

    modificacion

--- Lab02/Caja.java
+++ Lab02/Caja.java
@@ -1,5 +1,11 @@
 package Lab02;
 
-public class Caja {
-    
+public class Caja<T> {
+    private String color;
+    private T contenido;
+
+    public Caja(String color, T contenido) {
+        this.color = color;
+        this.contenido = contenido;
+    }
 }
