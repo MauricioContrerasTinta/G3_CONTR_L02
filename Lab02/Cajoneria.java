22ef1ec155cf4e270eb5999fe769115f59ba1ffa
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-03-27 23:58:13 -0500

    modificacion

---
+++ Lab02/Cajoneria.java
@@ -0,0 +1,19 @@
+package Lab02;
+import java.util.ArrayList;
+
+public class Cajoneria<T> {
+    private ArrayList<Caja<T>> cajas = new ArrayList<>();
+    public void addCaja(Caja<T> caja) {
+        cajas.add(caja);
+    }
+
+    public String search(T objeto) {
+        for (int i = 0; i < cajas.size(); i++) {
+            if (cajas.get(i).getContenido().equals(objeto)) {
+                return "Posición: " + (i + 1) + ", Color: " + cajas.get(i).getColor();
+            }
+        }
+        return "no encontrado";
+    }
+
+}
